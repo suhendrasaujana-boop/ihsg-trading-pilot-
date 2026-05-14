6fa9715300587f587d417f6673f87497e89ad524
suhendrasaujana-boop <272155887+suhendrasaujana-boop@users.noreply.github.com>
2026-05-14 20:50:18 +0700

    Update IHSG stock configuration and timeframes

--- config.py
+++ config.py
@@ -1,20 +1,153 @@
-# Konfigurasi Saham IHSG (Liquid Stocks)
+# Konfigurasi Saham IHSG (Lengkap)
 IHSG_STOCKS = {
+    # === BANKING (Sektor Perbankan) ===
     'BBCA.JK': 'Bank Central Asia',
-    'BBRI.JK': 'Bank Rakyat Indonesia', 
+    'BBRI.JK': 'Bank Rakyat Indonesia',
     'BMRI.JK': 'Bank Mandiri',
+    'BBNI.JK': 'Bank Negara Indonesia',
+    'BRIS.JK': 'Bank Syariah Indonesia',
+    'BNGA.JK': 'Bank CIMB Niaga',
+    'AGRO.JK': 'Bank Raya Indonesia',
+    'ARTO.JK': 'Bank Jago',
+    'BBKP.JK': 'Bank Bukopin',
+    'BDMN.JK': 'Bank Danamon',
+    'BJBR.JK': 'Bank Jabar Banten',
+    'BJTM.JK': 'Bank Jatim',
+    'BSIM.JK': 'Bank Sinarmas',
+    'BTPS.JK': 'Bank Tabungan Pensiunan',
+    
+    # === TELCO & TECHNOLOGY ===
     'TLKM.JK': 'Telkom Indonesia',
-    'ASII.JK': 'Astra International',
-    'UNVR.JK': 'Unilever Indonesia',
+    'ISAT.JK': 'Indosat Ooredoo',
+    'EXCL.JK': 'XL Axiata',
+    'FREN.JK': 'Smartfren Telecom',
+    'MTDL.JK': 'Metrodata Electronics',
+    'GOTO.JK': 'GoTo Gojek Tokopedia',
+    
+    # === ENERGY & MINING ===
     'ADRO.JK': 'Adaro Energy',
-    'ICBP.JK': 'Indofood CBP'
+    'BUMI.JK': 'Bumi Resources',
+    'ITMG.JK': 'Indo Tambangraya',
+    'PTBA.JK': 'Bukit Asam',
+    'BYAN.JK': 'Bayan Resources',
+    'MEDC.JK': 'Medco Energi',
+    'ELSA.JK': 'Elnusa',
+    'ENRG.JK': 'Energi Mega Persada',
+    'HRUM.JK': 'Harum Energy',
+    'INDY.JK': 'Indika Energy',
+    'KKGI.JK': 'Resource Alam Indonesia',
+    'MBAP.JK': 'Mitrabara Adiperdana',
+    'PKPK.JK': 'Perdana Karya Perkasa',
+    'SGER.JK': 'Sumber Global Energy',
+    'TOBA.JK': 'Toba Bara Sejahtra',
+    
+    # === CONSUMER GOODS ===
+    'UNVR.JK': 'Unilever Indonesia',
+    'ICBP.JK': 'Indofood CBP',
+    'INDF.JK': 'Indofood Sukses',
+    'MYOR.JK': 'Mayora Indah',
+    'KLBF.JK': 'Kalbe Farma',
+    'HMSP.JK': 'H.M. Sampoerna',
+    'GGRM.JK': 'Gudang Garam',
+    'DVLA.JK': 'Darya-Varia',
+    'KAEF.JK': 'Kimia Farma',
+    'MERK.JK': 'Merck Indonesia',
+    'PYFA.JK': 'Pyridam Farma',
+    'SIDO.JK': 'Industri Jamu Sido Muncul',
+    'TSPC.JK': 'Tempo Scan Pacific',
+    'ULTJ.JK': 'Ultra Jaya Milk',
+    'CINT.JK': 'Chitose Internasional',
+    'COCO.JK': 'Wahana Interfood',
+    'DLTA.JK': 'Delta Djakarta',
+    'MLBI.JK': 'Multi Bintang',
+    
+    # === AUTOMOTIVE & INDUSTRIAL ===
+    'ASII.JK': 'Astra International',
+    'AUTO.JK': 'Astra Otoparts',
+    'GDYR.JK': 'Goodyear Indonesia',
+    'INDS.JK': 'Indospring',
+    'PRAS.JK': 'Prima Alloy Steel',
+    
+    # === PROPERTY & REAL ESTATE ===
+    'BSDE.JK': 'Bumi Serpong Damai',
+    'CTRA.JK': 'Ciputra Development',
+    'PWON.JK': 'Pakuwon Jati',
+    'DILD.JK': 'Intiland Development',
+    'GPRA.JK': 'Perdana Gapuraprima',
+    'JRPT.JK': 'Jaya Real Property',
+    'KIJA.JK': 'Kawasan Industri Jababeka',
+    'LPCK.JK': 'Lippo Cikarang',
+    'LPKR.JK': 'Lippo Karawaci',
+    'MDLN.JK': 'Modernland Realty',
+    'PLIN.JK': 'Plaza Indonesia',
+    'SMRA.JK': 'Summarecon Agung',
+    'WIKA.JK': 'Wijaya Karya Bangunan Gedung',
+    
+    # === INFRASTRUCTURE ===
+    'ADHI.JK': 'Adhi Karya',
+    'PTPP.JK': 'PP Properti',
+    'WIKA.JK': 'Wijaya Karya',
+    'WSKT.JK': 'Waskita Karya',
+    'JSMR.JK': 'Jasa Marga',
+    'PGAS.JK': 'Perusahaan Gas Negara',
+    'TOWR.JK': 'Sarana Menara Nusantara',
+    'TBIG.JK': 'Tower Bersama',
+    
+    # === RETAIL & TRADE ===
+    'ACES.JK': 'Ace Hardware',
+    'ERAA.JK': 'Erajaya Swasembada',
+    'MAP.JK': 'Mitra Adiperkasa',
+    'MAPI.JK': 'Mitra Adiperkasa',
+    'AMRT.JK': 'Sumber Alfaria Trijaya',
+    'RALS.JK': 'Ramayana Lestari',
+    'LPPF.JK': 'Matahari Department Store',
+    'SONA.JK': 'Sona Topas',
+    
+    # === CEMENT ===
+    'INTP.JK': 'Indocement Tunggal',
+    'SMCB.JK': 'Semen Indonesia',
+    'SMBR.JK': 'Semen Baturaja',
+    
+    # === PLANTATION ===
+    'AALI.JK': 'Astra Agro Lestari',
+    'LSIP.JK': 'PP London Sumatra',
+    'SGRO.JK': 'Sampoerna Agro',
+    'SMAR.JK': 'Smartfren Telecom',
+    'UNSP.JK': 'Bakrie Sumatera Plantations',
+    
+    # === TRANSPORTATION ===
+    'BIRD.JK': 'Blue Bird',
+    'CMNP.JK': 'Citra Marga Nusaphala',
+    'GIAA.JK': 'Garuda Indonesia',
+    'SMDR.JK': 'Samudera Indonesia',
+    'TMAS.JK': 'Temas Line',
+    
+    # === CYCLICALS ===
+    'ANTM.JK': 'Aneka Tambang',
+    'INCO.JK': 'Vale Indonesia',
+    'TINS.JK': 'Timah',
+    'MDKA.JK': 'Merdeka Copper Gold',
+    
+    # === MISCELLANEOUS ===
+    'MREI.JK': 'Maskapai Reasuransi',
+    'PANI.JK': 'Pantai Indah Kapuk',
+    'SMMA.JK': 'Sinar Mas Multiartha',
+    'SQMI.JK': 'Renuka Coalindo',
+    'SRTG.JK': 'Saratoga Investama',
+    'TECH.JK': 'Indointernet',
+    'TRAM.JK': 'Trada Maritime',
+    'UNTR.JK': 'United Tractors',
 }
 
 # Timeframes untuk multi-timeframe analysis
 TIMEFRAMES = {
-    '1h': '60m',
-    '4h': '4h', 
-    '1d': '1d'
+    '5 Menit': '5m',
+    '15 Menit': '15m', 
+    '30 Menit': '30m',
+    '1 Jam': '1h',
+    '4 Jam': '4h',
+    '1 Hari': '1d',
+    '1 Minggu': '1wk'
 }
 
 # Scoring weights
